6e584e334c879d5bc69f1c412db448f9679f0c16
Thomas Bogart <trbogart@gmail.com>
2026-07-12 07:10:48 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    load_dotenv()
+    CLIENT_ID = os.getenv('CLIENT_ID')
+    CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+
+
